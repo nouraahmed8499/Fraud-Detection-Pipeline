6b90e6d538aaaf174f65c81b6e357c5f13c5c9b7
nouraahmed8499 <nourhanelsehrawy@gmail.com>
2025-07-25 15:01:06 +0300

    adding creation of fraud-detectio database

--- scripts/pkl_to_postgres.py
+++ scripts/pkl_to_postgres.py
@@ -1,7 +1,8 @@
 import os
 import glob
 import pandas as pd
-from sqlalchemy import create_engine
+import psycopg2
+from sqlalchemy import create_engine, text
 
 # pkl files directory
 PKL_DIR = '/opt/airflow/data' 
@@ -11,9 +12,37 @@ DB_USER = 'airflow'
 DB_PASSWORD = 'airflow'
 DB_HOST = 'postgres'
 DB_PORT = '5432'
-DB_NAME = 'airflow'
+DB_NAME = 'fraud-detection'
 DB_TABLE = 'transactions'
+DEFAULT_DB = 'postgres'
 
+def create_database_if_not_exists():
+    """
+    Connects to the default DB and creates the target DB if it does not exist.
+    """
+    conn = psycopg2.connect(
+        dbname=DEFAULT_DB,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}'")
+    exists = cur.fetchone()
+
+    if not exists:
+        cur.execute(f"CREATE DATABASE \"{DB_NAME}\"")
+        print(f"Database '{DB_NAME}' created.")
+    else:
+        print(f"Database '{DB_NAME}' already exists.")
+
+    cur.close()
+    conn.close()
+
+    
 def load_all_pickles(pkl_dir):
     """
     Load and combine all .pkl files in the specified directory
@@ -41,6 +70,7 @@ def insert_into_postgres(df):
     """
     conn_str = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     engine = create_engine(conn_str)
+    
 
     # Write to PostgreSQL
     df.to_sql(DB_TABLE, engine, if_exists='replace', index=False)
@@ -50,6 +80,7 @@ def main():
     """
     main function to load .pkl files and insert into PostgreSQL
     """
+    create_database_if_not_exists()
     df = load_all_pickles(PKL_DIR)
     insert_into_postgres(df)
 
